f29491803623e7956d646f72f06775be3a5370c3
Tr3itz <tiziano.tezze.career@gmail.com>
2026-04-12 21:39:10 +0200

    Managed absolute/episode time step split in case of episodes shorter than the simulation data

--- livinglab/base.py
+++ livinglab/base.py
@@ -7,9 +7,15 @@ from typing import Any, Optional, Mapping
 class Environment(ABC):
     def __init__(self, seed: Optional[int], start_time_step: int, end_time_step: int, episode_length: Optional[int]=None):
         self.seed = seed
+
+        # Simulation info
         self.start_time_step = start_time_step
         self.end_time_step = end_time_step
-        self.episode_length = episode_length
+
+        # Episodic info
+        self.episode_length = episode_length        
+        self.episodes = (self.end_time_step + 1) // self.episode_length
+        self.episode_counter = -1
 
     @property
     def seed(self):
@@ -17,7 +23,7 @@ class Environment(ABC):
     
     @property
     def time_step(self):
-        return self._time_step
+        return self._episode_start_time_step + self._episode_time_step
     
     @property
     def start_time_step(self):
@@ -31,6 +37,18 @@ class Environment(ABC):
     def episode_length(self):
         return self._episode_length
     
+    @property
+    def episodes(self):
+        return self._episodes
+
+    @property
+    def episode_counter(self):
+        return self._episode_counter
+    
+    @property
+    def episode_time_step(self):
+        return self._episode_time_step
+
     @property
     def episode_start_time_step(self):
         return self._episode_start_time_step
@@ -69,25 +87,43 @@ class Environment(ABC):
         if (self._end_time_step + 1) % self._episode_length != 0:
             print(f'[WARN] Episode length {self._episode_length} does not assure a full simulation coverage.')
 
+    @episodes.setter
+    def episodes(self, n: int):
+        assert n > 0, f'Invalid number of episodes n={n}. Must be > 0.'
+        self._episodes = n
+
+    @episode_counter.setter
+    def episode_counter(self, new_value: int):
+        assert new_value <= self.episodes, f'Episode counter exceeding maximum number of episodes (Counter={new_value} > Episodes={self._episodes}).'
+        self._episode_counter = new_value
+
+    @episode_time_step.setter
+    def episode_time_step(self, new_step: int):
+        assert new_step < self._episode_length, f'Episode running time step exceeding episode length (timestep={new_step} >= length={self._episode_length}).'
+        self._episode_time_step = new_step
+
     @episode_start_time_step.setter
     def episode_start_time_step(self, new_step: int):
-        assert self.start_time_step <= new_step <= self.end_time_step, \
-            f'Invalid episode start time step: {new_step} not in [{self.start_time_step}, {self.end_time_step}]'
+        assert self._start_time_step <= new_step <= self._end_time_step, \
+            f'Invalid episode start time step: {new_step} not in [{self._start_time_step}, {self._end_time_step}]'
         self._episode_start_time_step = new_step
     
     @episode_end_time_step.setter
     def episode_end_time_step(self, new_step: int):
-        assert self.start_time_step <= new_step <= self.end_time_step, \
-            f'Invalid episode end time step: {new_step} not in [{self.start_time_step}, {self.end_time_step}]'
+        assert self._start_time_step <= new_step <= self._end_time_step, \
+            f'Invalid episode end time step: {new_step} not in [{self._start_time_step}, {self._end_time_step}]'
         assert new_step > self._episode_start_time_step, \
             f'Invalid episode start/end steps (start={self._episode_start_time_step} >= end={new_step}).'
         self._episode_end_time_step = new_step
 
     def step(self):
-        self.time_step += 1
+        self.episode_time_step += 1
 
     def reset(self):
-        self.time_step = 0
+        self.episode_counter += 1
+        self.episode_time_step = 0
+        self.episode_start_time_step = self.episode_counter*self.episode_length
+        self.episode_end_time_step = self.episode_start_time_step + self.episode_length - 1
 
 
 class Device(Environment):
