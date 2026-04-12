ea6371ae9d34e506bfae55045ab524936de3046c
Tr3itz <tiziano.tezze.career@gmail.com>
2026-04-12 21:37:15 +0200

    Updated example script for initializing the environment

--- examples/init_env.py
+++ examples/init_env.py
@@ -2,17 +2,16 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import livinglab # <- enable env registration
-import gymnasium as gym
+from livinglab.envs import LivingLabEnv
 
 
 def init_env():
-    env = gym.make(
-        'LivingLab-v0',
+    env = LivingLabEnv(
         seed=42,
         path='../data/datasets/citylearn_challenge_2023_phase_1',
         start_time_step=0,
-        end_time_step=720,
+        end_time_step=719,
+        episode_length=24,
         heat_pump_cfgs={
             'nominal_power': 4.12,
             'efficiency': 0.25,
@@ -79,21 +78,55 @@ def init_env():
                 43.536755
             ],
         },
-        periodic_normalization=False
+        periodic_normalization=True
     )
 
+    return env
+
+
+def info():
+    env = init_env()
+
     print(f'##### LIVINGLAB ENV #####')
-    print(f'Observation names:\n{env.unwrapped.observation_names}\n')
+    print(f'#Episodes: {env.episodes} (length={env.episode_length})\n')
+    print(f'Observation names:\n{env.observation_names}\n')
     print(f'Observation space:\n{env.observation_space}\n')
-    print(f'Action names:\n{env.unwrapped.action_names}\n')
+    print(f'Action names:\n{env.action_names}\n')
     print(f'Action space:\n{env.action_space}')
     print('##########################')
 
-    obs, _ = env.reset()
-    print(f'\nObservation at time step {env.unwrapped.time_step}:\n{obs}')
+    _, _ = env.reset()
+    obs = env.observations(periodic_normalization=False, names=True)
+    print(f'\nObservation at time step {env.time_step}:\n{obs}')
+
+    actions = [0.0, 0.0]
+    _, reward, terminated, _, info = env.step(actions=actions)
+    print(f'\nAction {actions} result:\n - {env.reward_fn.__class__.__name__}: {reward}\n - Terminated: {terminated}\n - Info: {info}')
+
+    next_obs = env.observations(periodic_normalization=False, names=True)
+    print(f'\nObservation at time step {env.time_step}:\n{next_obs}')
+
+    env.close()
+
+
+def sanity_check():
+    env = init_env()
+
+    for _ in range(env.episodes):
+        env.reset()
+        while not env.terminated:
+            if env.episode_time_step % 2 == 0:
+                env.step(actions=[0.5, 1.0])
+            else:
+                env.step(actions=[0.2, -1.0])
+
+        print(f'\n[EPISODE {env.episode_counter}-({env.episode_start_time_step}:{env.episode_end_time_step})] LivingLabEnv terminated successfully after {env.episode_length} time steps.')
+        for k, v in env.info.items():
+            print(f'- {k}: {v}')
 
     env.close()
 
 
 if __name__ == '__main__':
-    init_env()
+    info()
+    sanity_check()
